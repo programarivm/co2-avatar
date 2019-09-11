8ab38cfca4e2d9ed584196bb158bcac1e5000f29
programarivm <info@programarivm.com>
2019-09-11 18:26:53 +0100

    a login form is added

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  Container,
-  Row,
-  Col
+  Button, Card, CardBody,
+  Container, Col, Row, Form,
+  FormGroup, Input
 } from 'reactstrap';
 
-class SignIn extends React.Component {
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+      'email': '',
+      'password': '',
+      validate: {
+        emailState: '',
+      },
+    }
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    console.log(`Email: ${ this.state.email }`)
+  }
+
   render() {
     return (
-      <div>
-        <Container>
-          <Row>
-            <Col lg="12">
-              <p>TODO</p>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Container>
+        <Row>
+          <Col lg="3"></Col>
+          <Col lg="6">
+            <Card>
+              <CardBody>
+                <h4>Log in to your account</h4>
+                <Form className="form" onSubmit={ (e) => this.submitForm(e) }>
+                  <FormGroup>
+                    <Input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="Email address"
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="Password"
+                    />
+                  </FormGroup>
+                  <Button>Log in</Button>
+                </Form>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="3"></Col>
+        </Row>
+      </Container>
     );
   }
 }
