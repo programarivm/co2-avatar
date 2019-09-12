8594228962bf7976cd213f89ce18c3ba98fbea4c
programarivm <info@programarivm.com>
2019-09-12 17:52:53 +0100

    the /auth endpoint is set up

--- api/index.php
+++ api/index.php
@@ -1,5 +1,13 @@
 <?php
 
-echo 'foo';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-exit;
+define('APP_PATH', realpath(dirname(__FILE__) . '/../'));
+
+$dotenv = \Dotenv\Dotenv::create(__DIR__.'/../');
+$dotenv->load();
+
+switch (true) {
+    case '/auth' === $_SERVER['REQUEST_URI']:
+        require_once APP_PATH.'/api/endpoints/auth.php';
+}
