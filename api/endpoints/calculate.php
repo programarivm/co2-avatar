5620794b605a3a3496d69bff23cebc9401b7bc53
programarivm <info@programarivm.com>
2019-09-16 11:44:23 +0100

    Authorizer class is added

--- api/endpoints/calculate.php
+++ api/endpoints/calculate.php
@@ -1,16 +1,9 @@
 <?php
 
 use Co2\Utils\DB;
-use Firebase\JWT\JWT;
+use Co2\Utils\Authorizer;
 
-try {
-    $jwt = JWT::decode($_COOKIE['access_token'], getenv('JWT_SECRET'), ['HS256']);
-} catch (\Exception $e) {
-    $body = ['message' => 'Unauthorized'];
-    http_response_code(401);
-    print_r(json_encode($body, true));
-    exit;
-}
+$jwt = Authorizer::authorize();
 
 http_response_code(204);
 
--- api/endpoints/points.php
+++ api/endpoints/points.php
@@ -1,16 +1,9 @@
 <?php
 
 use Co2\Utils\DB;
-use Firebase\JWT\JWT;
+use Co2\Utils\Authorizer;
 
-try {
-    $jwt = JWT::decode($_COOKIE['access_token'], getenv('JWT_SECRET'), ['HS256']);
-} catch (\Exception $e) {
-    $body = ['message' => 'Unauthorized'];
-    http_response_code(401);
-    print_r(json_encode($body, true));
-    exit;
-}
+$jwt = Authorizer::authorize();
 
 $sql = "SELECT * FROM points WHERE id_user={$jwt->id}";
 $result = DB::getInstance()->query($sql)->fetch_array(MYSQLI_ASSOC);
