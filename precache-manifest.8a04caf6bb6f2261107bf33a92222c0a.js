268c9c76089ff186dd1d9776989a873af72d3028
programarivm <info@programarivm.com>
2019-09-21 21:25:13 +0100

    Updates

---
+++ precache-manifest.8a04caf6bb6f2261107bf33a92222c0a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbd09424212e589d8b85387912940b4",
+    "url": "/co2-avatar/index.html"
+  },
+  {
+    "revision": "cfc31448b4817bfe661a",
+    "url": "/co2-avatar/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "746afba492bdcc71b084",
+    "url": "/co2-avatar/static/css/main.0d53d491.chunk.css"
+  },
+  {
+    "revision": "cfc31448b4817bfe661a",
+    "url": "/co2-avatar/static/js/2.44843a42.chunk.js"
+  },
+  {
+    "revision": "746afba492bdcc71b084",
+    "url": "/co2-avatar/static/js/main.1572f6e2.chunk.js"
+  },
+  {
+    "revision": "1fad641eca32b29aaac6",
+    "url": "/co2-avatar/static/js/runtime~main.abdeb7e2.js"
+  },
+  {
+    "revision": "2ec46c97c52ff158fd9d20ae332e5572",
+    "url": "/co2-avatar/static/media/arctic.2ec46c97.jpeg"
+  },
+  {
+    "revision": "4452f802f7ba9a74e315ff19c65ab74d",
+    "url": "/co2-avatar/static/media/bees.4452f802.jpg"
+  },
+  {
+    "revision": "2a54ba87696178858619d88466422cff",
+    "url": "/co2-avatar/static/media/earth.2a54ba87.svg"
+  },
+  {
+    "revision": "e77c97e59058bf48ad212b8ea41912a8",
+    "url": "/co2-avatar/static/media/healer.e77c97e5.jpg"
+  },
+  {
+    "revision": "69f3b8fa1a1619b4937a0976c02b005f",
+    "url": "/co2-avatar/static/media/hero.69f3b8fa.jpg"
+  },
+  {
+    "revision": "d9f5d8be51fc0c3797623232207bd54e",
+    "url": "/co2-avatar/static/media/london.d9f5d8be.jpg"
+  },
+  {
+    "revision": "33de7467113fca783aa9b8a0e05c8e8b",
+    "url": "/co2-avatar/static/media/mutant.33de7467.jpg"
+  },
+  {
+    "revision": "3e5520dfdf1799a25d5feb50492b7ec8",
+    "url": "/co2-avatar/static/media/neutral.3e5520df.jpg"
+  },
+  {
+    "revision": "0c63cbe656323400553322ee4ba2cfa8",
+    "url": "/co2-avatar/static/media/polluter.0c63cbe6.jpg"
+  }
+]);
