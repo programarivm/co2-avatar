60a53fdc34ddb5db2a4c289ecd831260f616a00c
programarivm <info@programarivm.com>
2019-09-21 14:30:03 +0100

    cleanup

--- src/components/private/TakeTest.js
+++ src/components/private/TakeTest.js
@@ -24,9 +24,7 @@ const track = {
 class TakeTest extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      questions: []
-    };
+    this.state = {};
     this.seeResults = this.seeResults.bind(this);
   }
 
@@ -39,10 +37,11 @@ class TakeTest extends React.Component {
       }
     }).then(res => res.json())
       .then(res => {
-        this.setState({ questions: JSON.parse(res) });
-        this.state.questions.sort(function() {
+        let questions = JSON.parse(res);
+        questions.sort(function() {
           return 0.5 - Math.random();
         });
+        this.setState({ questions: questions });
       });
   }
 
@@ -62,61 +61,65 @@ class TakeTest extends React.Component {
 
   render() {
     return (
-      <div>
-        <Container className="TakeTest">
-          <h1 className="mt-5">Slide the big squares</h1>
-          <Row>
-            <Col lg="12">
-              {
-                this.state.questions.map( (item, i) => <Row className="mt-5">
-                  <h5 className="m-4">{item.text}</h5>
-                  <Range
-                    step={0.1}
-                    min={0}
-                    max={100}
-                    values={item.values}
-                    onChange={values => {
-                      let newState = Object.assign({}, this.state);
-                      newState.questions[i].values = values;
-                      this.setState(newState);
-                    }}
-                    renderTrack={({ props, children }) => (
-                      <div
-                        {...props}
-                        style={{
-                          ...props.style,
-                          ...track.style
-                        }}
-                      >
-                        {children}
-                      </div>
-                    )}
-                    renderThumb={({ props }) => (
-                      <div
-                        {...props}
-                        style={{
-                          ...props.style,
-                          ...thumb.style
-                        }}
-                      />
-                    )}
-                  />
-                </Row> )
-              }
-              <Row className="m-5">
-                <Button
-                  className="btn-lg"
-                  color="primary"
-                  onClick={this.seeResults}
-                  block
-                >
-                See results
-                </Button>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Container className="TakeTest">
+        {
+          Object.keys(this.state).length === 0
+            ? <p className="mt-5">
+                Loading...
+              </p>
+            : <Row>
+                <Col lg="12">
+                <h1 className="mt-5">Slide the big squares</h1>
+                {
+                  this.state.questions.map( (item, i) => <Row className="mt-5">
+                    <h5 className="m-4">{item.text}</h5>
+                    <Range
+                      step={0.1}
+                      min={0}
+                      max={100}
+                      values={item.values}
+                      onChange={values => {
+                        let newState = Object.assign({}, this.state);
+                        newState.questions[i].values = values;
+                        this.setState(newState);
+                      }}
+                      renderTrack={({ props, children }) => (
+                        <div
+                          {...props}
+                          style={{
+                            ...props.style,
+                            ...track.style
+                          }}
+                        >
+                          {children}
+                        </div>
+                      )}
+                      renderThumb={({ props }) => (
+                        <div
+                          {...props}
+                          style={{
+                            ...props.style,
+                            ...thumb.style
+                          }}
+                        />
+                      )}
+                    />
+                  </Row> )
+                }
+                <Row className="m-5">
+                  <Button
+                    className="btn-lg"
+                    color="primary"
+                    onClick={this.seeResults}
+                    block
+                  >
+                  See results
+                  </Button>
+                </Row>
+              </Col>
+            </Row>
+        }
+      </Container>
     );
   }
 }
