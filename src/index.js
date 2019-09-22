01cc2026342ff050acf85322693d864baceee991
programarivm <info@programarivm.com>
2019-09-22 13:48:40 +0100

    BrowserRouter is replaced with HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
 // css
@@ -10,9 +10,9 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
